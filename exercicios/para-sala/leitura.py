d8cb6326ff1c8a7bec7b9c05b2d154a8114fc583
nataliabpo <nataliaoliveira0108@gmail.com>
2024-07-10 17:49:39 -0300

    Update leitura.py

--- exercicios/para-sala/leitura.py
+++ exercicios/para-sala/leitura.py
@@ -1,12 +1,14 @@
-import csv  # Importa o módulo csv, que fornece funcionalidades para ler e escrever arquivos CSV
+arquivo = open('maio_abril_2024.csv', 'w')
+arquivo.write("id,mean\n")
+csv_files = ['abril-2024.csv', 'maio2-2024.csv']
+for filename in csv_files:
+  with open(filename) as open_csv:
+      first_row = True  
+      for line in open_csv:
+          # Ignora a linha de cabeçalho
+          if first_row:
+              first_row = False
+              continue
+          arquivo.write(line.strip() + '\n')  
 
-# Abre o arquivo 'maio_abril_2024.csv' para leitura, usando o gerenciador de contexto 'with'
-# O parâmetro 'newline=""' é usado para evitar problemas com quebras de linha
-# O parâmetro 'encoding="utf-8"' garante que o arquivo seja lido usando a codificação UTF-8
-with open('maio_abril_2024.csv', newline='', encoding='utf-8') as csvfile:
-    leitor = csv.reader(csvfile)  # Cria um objeto leitor CSV que itera sobre as linhas do arquivo
-
-    # Itera sobre cada linha do objeto leitor CSV
-    for linha in leitor:
-        print(linha)  # Imprime cada linha lida do arquivo CSV
-        # Cada linha é uma lista de strings, onde cada elemento da lista corresponde a um campo no CSV
+arquivo.close()
