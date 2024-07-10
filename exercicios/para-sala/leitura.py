78a38db5cb41b988b7a5eb14adf6b6287adc8a1d
nataliabpo <nataliaoliveira0108@gmail.com>
2024-07-10 17:50:11 -0300

    Update leitura.py

--- exercicios/para-sala/leitura.py
+++ exercicios/para-sala/leitura.py
@@ -1,14 +1,6 @@
-arquivo = open('maio_abril_2024.csv', 'w')
-arquivo.write("id,mean\n")
-csv_files = ['abril-2024.csv', 'maio2-2024.csv']
-for filename in csv_files:
-  with open(filename) as open_csv:
-      first_row = True  
-      for line in open_csv:
-          # Ignora a linha de cabeçalho
-          if first_row:
-              first_row = False
-              continue
-          arquivo.write(line.strip() + '\n')  
+import csv  
 
-arquivo.close()
+with open('maio_abril_2024.csv', newline='', encoding='utf-8') as csvfile:
+    leitor = csv.reader(csvfile) 
+    for linha in leitor:
+        print(linha)
