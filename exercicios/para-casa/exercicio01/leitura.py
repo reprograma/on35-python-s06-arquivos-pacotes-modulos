905d90e3035b1d84fa381ebb28356e5009d28d9c
Daniele Napoleao <daniele20@gmail.com>
2024-07-12 18:35:36 -0300

    Exercicios Excel I / Merge Python

--- exercicios/para-casa/exercicio01/leitura.py
+++ exercicios/para-casa/exercicio01/leitura.py
@@ -1,12 +1,6 @@
-import csv  # Importa o módulo csv, que fornece funcionalidades para ler e escrever arquivos CSV
-
-# Abre o arquivo 'maio_abril_2024.csv' para leitura, usando o gerenciador de contexto 'with'
-# O parâmetro 'newline=""' é usado para evitar problemas com quebras de linha
-# O parâmetro 'encoding="utf-8"' garante que o arquivo seja lido usando a codificação UTF-8
+import csv  
 with open('exercicio01_t1_t2.csv', newline='', encoding='utf-8') as csvfile:
-    leitor = csv.reader(csvfile)  # Cria um objeto leitor CSV que itera sobre as linhas do arquivo
+    leitor = csv.reader(csvfile)  
 
-    # Itera sobre cada linha do objeto leitor CSV
     for linha in leitor:
-        print(linha)  # Imprime cada linha lida do arquivo CSV
-        # Cada linha é uma lista de strings, onde cada elemento da lista corresponde a um campo no CSV
+        print(linha)  
